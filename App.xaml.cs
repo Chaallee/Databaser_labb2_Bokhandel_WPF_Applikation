bf7c50f17be27ee07cec25b75359d58ca61076da
Charlie Carlegrund <charlie_carlegrund@hotmail.com>
2026-01-12 11:11:26 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Bokhandel_WPF_Applikation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
